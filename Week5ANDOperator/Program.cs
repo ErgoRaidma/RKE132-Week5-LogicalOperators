602750c228e2ed5a1a2306981c0cc734cb70b81b
Ergo <ergo.raidma@tktk.ee>
2024-02-08 20:54:05 +0200

    create new app

---
+++ Week5ANDOperator/Program.cs
@@ -0,0 +1,21 @@
+﻿//Math>= 90, Biology >=90, Chemistry >=90
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your Math resault:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Biology resault:");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry resault:");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if (math >= 90 && biology >= 90 && chemistry >= 90) 
+{
+    Console.WriteLine("Congratulation! You are accepted");
+}
+else 
+{
+    Console.WriteLine("Your application cannot be approved");
+}
---
+++ Week5OROperator/Program.cs
@@ -0,0 +1,26 @@
+﻿//Math => 90 OR Chemistry => 90 OR Biologu >= 90
+
+//math && chemistry || math && biology || chemisrty && biology >=90
+
+using System;
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your Math resault:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your biology resault");
+biology = Int32.Parse(Console.ReadLine());  
+
+Console.WriteLine("Enter your Chemistry resault:");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if(math >= 90 && chemistry >= 90 || (math >= 90 && biology >= 90) || (chemistry >= 90 && biology >= 90))
+
+{
+    Console.WriteLine("Congratulation! You are accepted");
+}
+else
+{
+   Console.WriteLine("Your application cannot be approved");
+}
